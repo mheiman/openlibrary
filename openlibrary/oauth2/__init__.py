297403852fbb3d168887ffa412b6028ef30baf7c
Mark Heiman <github@loomis-house.com>
2025-12-30 16:12:10 -0800

    Create OAuth2 module initialization file

---
+++ openlibrary/oauth2/__init__.py
@@ -0,0 +1,17 @@
+"""
+OAuth2 Module
+
+This module provides OAuth2 authentication support for OpenLibrary.
+"""
+
+__version__ = "0.1.0"
+__author__ = "OpenLibrary Contributors"
+
+# Import OAuth2 components when they become available
+# from .client import OAuth2Client
+# from .provider import OAuth2Provider
+# from .utils import OAuth2Utils
+
+__all__ = [
+    # Add public API exports here
+]
